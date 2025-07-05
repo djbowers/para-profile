6140915e6408820c367db32a070009d622cbf2e9
DJ Bowers <daniel_bowers@icloud.com>
2025-07-05 10:45:55 -0500

    refactor: update UI components and reorganize utils structure
    - Update all shadcn/ui components with latest configurations
    - Move utils.ts from lib/ to utils/ directory and rename to classNames.ts
    - Update components.json configuration
    - Modify utils/index.ts and vitest.config.ts for better organization

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export { getIconByCategory } from './icons'
 export { getProgressColor, getLevelColor } from './colors'
 export { getDataByType, setDataByType } from './data'
+export { cn } from './classNames'
